41d83af86e5f5b353744d9fb405a164ccbf4326d
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-07 18:10:09 -0600

    added chatSend

--- app/services/post.js
+++ app/services/post.js
@@ -1,2 +1,11 @@
 /* John McCutchan ========================================= */
 console.log("Post Services-on");
+app.service('PostServices', function($http, $q){
+  return {
+    chatSend: function(obj){
+      return $http
+      .post('https://velocilinx-chat.firebaseio.com/chats/.json', JSON.stringify(obj))
+      .then(console.log("Sent to Firebase: ", obj))
+    }
+  }
+})
