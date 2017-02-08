f6b07945f4a38683d62eea4c89d2be262efb14a9
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-08 02:02:17 -0600

    removed if split, and went with just post

--- app/services/post.js
+++ app/services/post.js
@@ -2,25 +2,17 @@
 console.log("Post Services-on");
 app.service('PostServices', function($http, $q, $rootScope){
   return {
+    chatSend: function(obj){
+      return $http
+        .post('https://velocilinx-chat.firebaseio.com/chats/.json', JSON.stringify(obj))
+        .then(console.log("Post to Firebase: ", obj))
+    },
     getChat: function(chat){
       return $http
       .get('https://velocilinx-chat.firebaseio.com/chats/.json')
       .then((response) => {
         return  response.data
       })
-    },
-    chatSend: function(obj){
-      for(var i in $rootScope.list){
-        if($rootScope.list.uid === $rootScope.uid){
-          return $http
-          .patch('https://velocilinx-chat.firebaseio.com/chats/.json', JSON.stringify(obj))
-          .then(console.log("Patched to Firebase: ", obj))
-        } else {
-          return $http
-          .post('https://velocilinx-chat.firebaseio.com/chats/.json', JSON.stringify(obj))
-          .then(console.log("Post to Firebase: ", obj))
-        }
-      }
     }
   }
 })
