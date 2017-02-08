9aac2c6af086466c329e99d692531391cb9cc7fd
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-07 18:08:50 -0600

    added logout

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,4 +1,28 @@
 /* John McCutchan ========================================= */
 console.log('ChatCtrl-on');
-app.controller('ChatCtrl', function($scope){ // arg 1 is name of control, arg 2 is function with scope name
+app.controller('ChatCtrl', function($location, $scope, $rootScope, PostServices, LoginServices, userUID){ // arg 1 is name of control, arg 2 is function with scope name
+  $scope.name = $rootScope.name;
+  //get uid and assign it to $scope
+  $scope.uid = userUID.uid()
+  console.log("$scope UID: ",$scope.uid)
+  //assign local $scope uid to global scope
+  $rootScope.uid = $scope.uid
+  console.log("$rootScope UID: ",$rootScope.uid)
+  //check to see name is global
+  console.log("Test2: ", $rootScope.name)
+  //build chat object
+  $scope.chat = {
+    "uid":$rootScope.uid,
+    "name": $scope.name,
+    "conversation": $scope.conversation,
+  }
+  //function to post chat to firebase
+  $scope.chatSend = ()=> {
+    PostServices
+    .chatSend($scope.chat)
+  }
+  $scope.logout = ()=> {
+    LoginServices
+    .logout()
+  }
 })
