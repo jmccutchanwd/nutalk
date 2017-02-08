bf9b19f3d795ddd62ea0b854c10aac3231cc6c99
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-08 01:16:06 -0600

    tweaked if statement

--- app/services/post.js
+++ app/services/post.js
@@ -1,11 +1,26 @@
 /* John McCutchan ========================================= */
 console.log("Post Services-on");
-app.service('PostServices', function($http, $q){
+app.service('PostServices', function($http, $q, $rootScope){
   return {
-    chatSend: function(obj){
+    getChat: function(chat){
       return $http
-      .post('https://velocilinx-chat.firebaseio.com/chats/.json', JSON.stringify(obj))
-      .then(console.log("Sent to Firebase: ", obj))
+      .get('https://velocilinx-chat.firebaseio.com/chats/.json')
+      .then((response) => {
+        return  response.data
+      })
+    },
+    chatSend: function(obj){
+      for(var i in $rootScope.list){
+        if($rootScope.list.uid === $rootScope.uid){
+          return $http
+          .patch('https://velocilinx-chat.firebaseio.com/chats/.json', JSON.stringify(obj))
+          .then(console.log("Patched to Firebase: ", obj))
+        } else {
+          return $http
+          .post('https://velocilinx-chat.firebaseio.com/chats/.json', JSON.stringify(obj))
+          .then(console.log("Post to Firebase: ", obj))
+        }
+      }
     }
   }
 })
