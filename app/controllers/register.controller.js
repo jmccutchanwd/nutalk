33879939795650056c871d6a051b47a7f8cd221e
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-16 04:10:04 -0600

    added nav buttons

--- app/controllers/register.controller.js
+++ app/controllers/register.controller.js
@@ -1,6 +1,6 @@
 /* John McCutchan ========================================= */
 console.log('MainCtrl-on');
 angular.module('UserApp')
-  .controller('registerCtrl', function(){
+  .controller('RegisterCtrl', function(){
     var registerCtrl = this;
   })
