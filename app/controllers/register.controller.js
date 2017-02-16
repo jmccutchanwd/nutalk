df833dcb4b63eef2fda35b3cbe34bc5d69bd97e0
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-16 03:16:33 -0600

    added controller

---
+++ app/controllers/register.controller.js
@@ -0,0 +1,6 @@
+/* John McCutchan ========================================= */
+console.log('MainCtrl-on');
+angular.module('UserApp', ['firebase', 'ui.router'])
+  .controller('registerCtrl', function(){
+    var registerCtrl = this;
+  })
