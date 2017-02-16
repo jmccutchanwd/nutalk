0128be49c367f1af271af558e01df5187dede938
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-16 02:30:23 -0600

    pruned code and dependencies

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,19 +1,4 @@
 /* John McCutchan ========================================= */
 console.log('LoginCtrl-on');
-app.controller('LoginCtrl', function($scope, LoginServices,$location,$rootScope){
-  $scope.login = () => {
-    $rootScope.name = $scope.name;// assigns name to global variable
-    LoginServices
-    .login($scope.email, $scope.password)
-    .then (console.log("Signed In"))
-    .then(() => $location.url('/chat'))
-  }
-  $scope.logout = () => {
-    LoginServices
-    .logout()
-    .then(()=> $location.url('/'))
-  }
-  $scope.cancel = function(){
-      $location.url('/')
-    }
+app.controller('LoginCtrl', function(){
 })
