4897d24c19b3fd8d4ce623af7b831d2f65f4bd9f
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-16 04:12:46 -0600

    setup auth variable with

--- app/services/login.js
+++ app/services/login.js
@@ -1,6 +1,7 @@
 /* John McCutchan ========================================= */
 console.log("Login services-on");
 angular.module('UserApp')
-  .service('LoginServices', function(){
-
+  .service('LoginServices', function($firebaseAuth){
+    var auth = $firebaseAuth();
+    return auth;
 })
