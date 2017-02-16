59e84ebedf33998d18d8a322f4aaf9b2e496a39a
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-16 04:31:58 -0600

    added login and register functions

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -1,6 +1,26 @@
 /* John McCutchan ========================================= */
 console.log('LoginCtrl-on');
 angular.module('UserApp')
-  .controller('LoginCtrl', function(){
+  .controller('LoginCtrl', function(Login, $state){
     var loginCtrl = this;
-  })
+    loginCtrl.user = {
+      email: '',
+      password: ''
+    }
+    loginCtrl.login = function(){
+      Login.$signInWithEmailAndPassword(loginCtrl.user.email, loginCtrl.user.password)
+      .then(function(login){
+        $state.go('home');
+      }, function(error){
+        loginCtrl.error = error;
+      });
+    };
+    loginCtrl.register = function(){
+      Login.$createUserWithEmailAndPassword(loginCtrl.user.email, loginCtrl.usser.pasword)
+      .then(function(user){
+        loginCtrl.login();
+      }, function(error){
+        loginCtrl.error = error;
+      });
+    };
+  });
