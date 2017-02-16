97f5c356e3d5f2ec980595aad5e21f83741b6eb4
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-16 02:30:34 -0600

    pruned code and dependencies

--- app/services/login.js
+++ app/services/login.js
@@ -1,15 +1,5 @@
 /* John McCutchan ========================================= */
 console.log("Login services-on");
-app.service('LoginServices', function($http, $q, $location){
-
-  return {
-    login: function(email, pass){
-      return $q.resolve(firebase.auth().createUserWithEmailAndPassword(email, pass))
-    },
-    logout: function(){
-      var user = firebase.auth().currentUser;
-      return user.delete()
-    }
-  }
+app.service('LoginServices', function(){
 
 })
