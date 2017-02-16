7f14ffd2f8eb222a7ce7352fd0c40498cd5ee206
John McCutchan <john.mccutchan.wd@gmail.com>
2017-02-16 02:29:14 -0600

    pruned code and dependencies

--- app/services/post.js
+++ app/services/post.js
@@ -1,18 +1,5 @@
 /* John McCutchan ========================================= */
 console.log("Post Services-on");
-app.service('PostServices', function($http, $q, $rootScope){
-  return {
-    chatSend: function(obj){
-      return $http
-        .post('https://velocilinx-chat.firebaseio.com/chats/.json', JSON.stringify(obj))
-        .then(console.log("Post to Firebase: ", obj))
-    },
-    getChat: function(chat){
-      return $http
-      .get('https://velocilinx-chat.firebaseio.com/chats/.json')
-      .then((response) => {
-        return  response.data
-      })
-    }
-  }
+app.service('PostServices', function(){
+
 })
